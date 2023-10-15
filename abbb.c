0805fac082c5f0caf752a44ff6b8c7b543c72155
Jatin-Jaiswal <144318740+Jatin-Jaiswal@users.noreply.github.com>
2023-10-15 20:28:14 +0530

    Add files via upload

---
+++ abbb.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+   printf("\t\t >2<\t\t");
+   printf("\n\t         / \\\t\n");
+   printf("\t        // \\\\\t");
+   printf("\n\t       //   \\\\\t");
+   printf("\n\t      //     \\\\\t");
+    printf("\n\t     //       \\\\\t");
+    printf("\n\t    //         \\\\\t");
+    return 0;
+}
